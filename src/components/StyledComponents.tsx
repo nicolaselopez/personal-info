caab005cfd0bc7cd291e9ca6f51a94bf23377c7e
Nicolas Ezequiel Lopez <nicolas.e.lopez@gmail.com>
2023-12-02 04:59:08 -0300

    Adds ScrollView to StyledComponents

--- src/components/StyledComponents.tsx
+++ src/components/StyledComponents.tsx
@@ -1,8 +1,13 @@
 import { styled } from 'nativewind';
-import { View as BaseView, Text as BaseText } from 'react-native';
-import { TextInput as BaseTextInput, Button as BaseButton } from 'react-native-paper';
+import { View as BaseView, ScrollView as BaseScrollView } from 'react-native';
+import {
+  TextInput as BaseTextInput,
+  Button as BaseButton,
+  Text as BaseText,
+} from 'react-native-paper';
 
 export const View = styled(BaseView);
 export const Text = styled(BaseText);
 export const TextInput = styled(BaseTextInput);
 export const Button = styled(BaseButton);
+export const ScrollView = styled(BaseScrollView);
