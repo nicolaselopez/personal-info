7d954df05f559ef6aebf857f932f1266836ff78b
Nicolas Ezequiel Lopez <nicolas.e.lopez@gmail.com>
2023-12-02 04:56:58 -0300

    Adds dark theme capabilities

--- App.tsx
+++ App.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { PaperProvider } from 'react-native-paper';
+import { PaperProvider, DefaultTheme } from 'react-native-paper';
 import { AuthProvider } from './src/providers/Auth';
-import { Main } from './src/pages/Main';
+import { MainRouter } from './src/navigation/MainRouter';
+import { NavigationContainer } from '@react-navigation/native';
+import { useColorScheme } from 'react-native';
 
 export default function App() {
+  const scheme = useColorScheme();
+
+  const darkTheme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: '#1A1A1A',
+      accent: '#FAFAFA',
+    },
+  };
+
   return (
-    <AuthProvider>
-      <PaperProvider>
-        <Main />
-      </PaperProvider>
-    </AuthProvider>
+    <NavigationContainer>
+      <AuthProvider>
+        <PaperProvider theme={scheme === 'dark' ? darkTheme : undefined}>
+          <MainRouter />
+        </PaperProvider>
+      </AuthProvider>
+    </NavigationContainer>
   );
 }
