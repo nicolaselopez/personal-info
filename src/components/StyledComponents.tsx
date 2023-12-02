453fe83ea7294ed950c46faac9c59563b8804848
Nicolas Ezequiel Lopez <nicolas.e.lopez@gmail.com>
2023-12-02 02:32:24 -0300

    Adds StyledComponents to allow TailwindCSS

---
+++ src/components/StyledComponents.tsx
@@ -0,0 +1,8 @@
+import { styled } from 'nativewind';
+import { View as BaseView, Text as BaseText } from 'react-native';
+import { TextInput as BaseTextInput, Button as BaseButton } from 'react-native-paper';
+
+export const View = styled(BaseView);
+export const Text = styled(BaseText);
+export const TextInput = styled(BaseTextInput);
+export const Button = styled(BaseButton);
